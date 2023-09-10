fab554c09c1ed1edf6be1659c6eddf726c7e2bed
Augusto Bellanich <augustobellanich@outlook.com>
2023-09-10 01:38:27 -0300

    Agregar archivos de proyecto.

---
+++ SensorM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SensorM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SensorM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SensorM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
